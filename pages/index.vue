37cb810cfba9d46edf6bf84806dd4b6c1e063352
Julie <julie.albini@edu.univ-fcomte.fr>
2024-07-12 21:56:01 +0200

    feat(ball) : début de création d'une ball

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <Background class="fond" />
+  <div class="content">
+    <Ball />
+  </div>
+</template>
+
+<style lang="scss">
+.fond {
+  z-index: 0;
+  position: absolute;
+}
+
+.content {
+  z-index: 50;
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
+
+<script setup></script>
